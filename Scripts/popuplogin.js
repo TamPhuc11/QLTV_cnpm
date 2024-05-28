5fe4f29a133028fc19b3e5a8063f00fdbdba6d64
TamPhuc11 <phucnguyen1452004@gmail.com>
2024-05-28 20:55:41 +0700

    PhucDepTraiVCLLamWebQuanLyThuVien

---
+++ Scripts/popuplogin.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var loginBtn = document.getElementById('loginBtn');
+    var loginPopup = document.getElementById('loginPopup');
+    var closeBtn = document.querySelector('.close-btn');
+
+    loginBtn.addEventListener('click', function () {
+        loginPopup.style.display = 'flex';
+    });
+
+    closeBtn.addEventListener('click', function () {
+        loginPopup.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === loginPopup) {
+            loginPopup.style.display = 'none';
+        }
+    });
+});
